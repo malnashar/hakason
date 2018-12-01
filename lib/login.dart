ae1fdec63ec8a0b4b439054e2236defb00e97e06
alnashar <alnasharm778@gmail.com>
2018-12-01 15:48:48 +0200

    add

---
+++ lib/login.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new LoginState();
+  }
+}
+class LoginState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return _getScuffold();
+  }
+
+
+
+    Widget _getScuffold(){
+    return new Scaffold();
+      // your code
+      
+  }
+  }
